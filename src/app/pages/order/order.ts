f9ae0a658e5c22e99f19f0298c2b9d1be1ef9770
Оля <71034985+dron-olya7@users.noreply.github.com>
2026-08-31 14:57:09 +0500

    Add files via upload

--- src/app/pages/order/order.ts
+++ src/app/pages/order/order.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { Header } from '../../header/header';
 import { DELIVERY_SIZES, DELIVERY_SPEEDS } from './order.config';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { UpperCasePipe } from '@angular/common';
+import { UpperCasePipe, NgIf } from '@angular/common';
+import { DeliveryApi } from '../../services/delivery-api';
 
 declare var ymaps: any;
 
 @Component({
   selector: 'app-order',
-  imports: [Header, UpperCasePipe, ReactiveFormsModule],
+  imports: [Header, UpperCasePipe, ReactiveFormsModule, NgIf],
   templateUrl: './order.html',
   styleUrl: './order.css',
 })
@@ -24,8 +25,9 @@ export class Order {
 
   public orderId: any = signal(null);
   public calculationResult: any = signal(null);
+  public isLoading = signal(false);
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private deliveryApi: DeliveryApi) {
     this.routeForm = this.formBuilder.group({
       from: ['', Validators.required],
       to: ['', Validators.required],
@@ -63,8 +65,10 @@ export class Order {
 
   public calculate() {
     this.calculationResult.set(null);
+    this.isLoading.set(true);
 
     if (!this.map || this.routeForm.invalid) {
+      this.isLoading.set(false);
       return;
     }
 
@@ -112,6 +116,7 @@ export class Order {
           total,
           speed
         });
+        this.isLoading.set(false);
       } catch (err) {
         this.failedCalculation();
       }
@@ -122,6 +127,7 @@ export class Order {
 
   private failedCalculation() {
     this.calculationResult.set(null);
+    this.isLoading.set(false);
     alert('Не удалось построить маршрут. Проверьте адреса и выбранные параметры.');
   }
 
@@ -148,8 +154,13 @@ export class Order {
       createdAt: new Date().toISOString()
     };
 
-    console.log(payload);
-    this.orderId.set(1);
+    this.deliveryApi.createDelivery(payload).subscribe((response) => {
+      if ('error' in response) {
+        alert(response.error);
+        return;
+      }
+
+      this.orderId.set(response.id);
+    });
   }
 }
-
